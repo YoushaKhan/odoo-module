71df96b30f0ab624f4d7a0e8b5bc081c02ee8011
YoushaKhan <86527559+YoushaKhan@users.noreply.github.com>
2022-07-30 00:39:28 +0500

    Delete patient.py

--- models/patient.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models,fields
-
-class Patient(models.Model):
-     _name= "hospital.patient"
-    patient_name = fields.Char(string='Name', required=True)
-    patient_note= fields.char(string='Note')
-    patient_age = fields.Integer('Age')
